479f1f1c7991c26386b19d3c11877db1671f5f65
lou <dustyfingers01257@gmail.com>
2024-04-04 23:38:13 -0600

    make cart product responsive

--- app/components/CartSummary/CartSummary.tsx
+++ app/components/CartSummary/CartSummary.tsx
@@ -1,72 +1,9 @@
-import React, { useEffect } from "react";
-// import Image from "next/image";
+import React from "react";
 
 import { useCartContext } from "@/app/context/Cart";
-import { CartItem } from "@/app/types/cart.types";
 import styles from "./component.module.css";
 import Link from "next/link";
-
-const CartDisplayItem = (props: CartItem) => {
-  const { name, price, quantity, image } = props;
-  const [cart, setCart] = useCartContext();
-
-  const editProductInCart = (product: CartItem) => {
-    console.log("hello!");
-  };
-
-  const removeProductFromCart = (product: CartItem) => {
-    const otherItemsInCart = cart.cartItems.filter(
-      (cartProduct) => cartProduct.id !== product.id
-    );
-    const totalCartItemsQuantity = otherItemsInCart.reduce(
-      (runningTotal, current) => {
-        return (runningTotal += current.quantity);
-      },
-      1
-    );
-    setCart({
-      cartItems: [...otherItemsInCart],
-      totalCartItemsQuantity,
-    });
-  };
-  console.log(quantity);
-
-  return (
-    <div className={styles.display_item}>
-      <div className={styles.top_row}>
-        <div className={styles.info_section}>
-          {name && <h3 className={styles.name}>{name}</h3>}
-          {price && <span className={styles.price}>${price}</span>}
-          {quantity && <p className={styles.quantity}>Qty: {quantity}</p>}
-        </div>
-        {/* {image && (
-          <Image
-            src={image}
-            width={500}
-            height={500}
-            alt={`picture of ${name}`}
-            className={styles.cart_display_item_image}
-          />
-        )} */}
-      </div>
-
-      <div className={styles.button_row}>
-        <button
-          className={styles.edit_cart_display_item_button}
-          onClick={() => editProductInCart({ ...props })}
-        >
-          edit
-        </button>
-        <button
-          className={styles.add_to_cart_button}
-          onClick={() => removeProductFromCart({ ...props })}
-        >
-          remove
-        </button>
-      </div>
-    </div>
-  );
-};
+import CartSummaryItem from "../CartSummaryItem";
 
 const CartSummary = () => {
   const [cart] = useCartContext();
@@ -74,7 +11,7 @@ const CartSummary = () => {
     <div className={styles.cart_summary}>
       {cart?.cartItems.length
         ? cart?.cartItems.map((cartItem) => (
-            <CartDisplayItem
+            <CartSummaryItem
               {...cartItem}
               key={cartItem.name + Math.round(Math.random() * 100)}
             />
---
+++ app/components/CartSummaryItem/CartSummaryItem.tsx
@@ -0,0 +1,69 @@
+import { CartItem } from "@/app/types/cart.types";
+import Image from "next/image";
+
+import { useCartContext } from "@/app/context/Cart";
+import styles from "./component.module.css";
+
+const CartSummaryItem = (props: CartItem) => {
+  const { name, price, quantity, image } = props;
+  const [cart, setCart] = useCartContext();
+
+  const editProductInCart = (product: CartItem) => {
+    console.log("hello!");
+  };
+
+  const removeProductFromCart = (product: CartItem) => {
+    const otherItemsInCart = cart.cartItems.filter(
+      (cartProduct) => cartProduct.id !== product.id
+    );
+    const totalCartItemsQuantity = otherItemsInCart.reduce(
+      (runningTotal, current) => {
+        return (runningTotal += current.quantity);
+      },
+      1
+    );
+    setCart({
+      cartItems: [...otherItemsInCart],
+      totalCartItemsQuantity,
+    });
+  };
+
+  return (
+    <div className={styles.display_item}>
+      <div className={styles.top_row}>
+        <div className={styles.info_section}>
+          {name && <h3 className={styles.name}>{name}</h3>}
+          {price && <span className={styles.price}>${price}</span>}
+          {quantity && <p className={styles.quantity}>Qty: {quantity}</p>}
+        </div>
+        {image && (
+          <Image
+            src={image}
+            width={500}
+            height={500}
+            alt={`picture of ${name}`}
+            className={styles.cart_display_item_image}
+            layout="responsive"
+          />
+        )}
+      </div>
+
+      <div className={styles.button_row}>
+        <button
+          className={styles.edit_cart_display_item_button}
+          onClick={() => editProductInCart({ ...props })}
+        >
+          edit
+        </button>
+        <button
+          className={styles.add_to_cart_button}
+          onClick={() => removeProductFromCart({ ...props })}
+        >
+          remove
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartSummaryItem;
