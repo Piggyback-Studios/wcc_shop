8cf361da136dd4a8ce256ac78cc5fabfb4678387
lou <dustyfingers01257@gmail.com>
2024-11-11 10:25:21 -0700

    complete product detail page

--- src/components/ProductDetailDisplay/ProductDetailDisplay.tsx
+++ src/components/ProductDetailDisplay/ProductDetailDisplay.tsx
@@ -1,6 +1,10 @@
-import { ProductDetailDisplayProps } from "@/src/shared/types";
 import Image from "next/image";
 import Link from "next/link";
+
+import CustomButton from "@/src/components/common/CustomButton";
+import { ProductDetailDisplayProps, Product } from "@/src/shared/types";
+import { useCartContext } from "@/src/context/Cart";
+import toast from "@/src/utils/toast";
 
 const ProductDetailDisplay = ({
   id,
@@ -8,7 +12,36 @@ const ProductDetailDisplay = ({
   description,
   imageUrl,
   price,
+  stripeId,
+  priceId,
+  stockQuantity,
+  cartQuantity,
 }: ProductDetailDisplayProps) => {
+  const [cart, setCart] = useCartContext();
+  const addProductToCart = (product: Product) => {
+    // i dont necessarily like this solution
+    const itemInCart = cart.cartProducts.filter(
+      (cartProduct) => cartProduct.id === product.id
+    )[0];
+    const otherItemsInCart = cart.cartProducts.filter(
+      (cartProduct) => cartProduct.id !== product.id
+    );
+
+    setCart({
+      cartProducts: [
+        ...otherItemsInCart,
+        {
+          ...product,
+          cartQuantity: itemInCart ? itemInCart.cartQuantity + 1 : 1,
+        } as Product,
+      ],
+    });
+  };
+
+  const handleClick = (product: Product) => {
+    addProductToCart(product);
+    toast(`${product.name} added to cart`, "success");
+  };
   return (
     <div className="grid gap-8">
       <div className="flex justify-between items-center">
@@ -26,10 +59,26 @@ const ProductDetailDisplay = ({
             />
           )}
         </div>
-        <div>
+        <div className="flex flex-col items-start gap-8">
           <p>{name}</p>
           <p>{price}</p>
           <p>{description}</p>
+          <CustomButton
+            onClick={() =>
+              handleClick({
+                id,
+                name,
+                price,
+                description,
+                imageUrl,
+                stripeId,
+                priceId,
+                stockQuantity,
+                cartQuantity,
+              } as Product)
+            }
+            label="Add to Cart"
+          />
         </div>
       </div>
     </div>
