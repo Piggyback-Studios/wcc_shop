d100584730538c8985b031979f5aa94a310d6620
lou <dustyfingers01257@gmail.com>
2024-11-13 16:48:31 -0700

    write more todos

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -7,12 +7,15 @@ import { getSession } from "@/src/utils/auth";
 import db from "@/src/utils/data/db";
 import { parseBool } from "@/src/utils/parse";
 
+// MASTER TODO MVP LIST
 // TODO: once purchased, remove the appropriate amount from stock quantity
 // TODO: dont allow users to purchase more than the stock quantity
 // TODO: once a purchase is completed, create an order in the database
 // TODO: once an order is shipped, admins and owners should be able to mark order as shipped in the system and the customer should get an email
 // TODO: allow admins to see all orders
 // TODO: make cart store in local storage
+// TODO: protect all necessary backend endpoints
+// TODO: write tests for api endpoints and block-level components
 
 // fetch all products
 export async function GET(req: NextRequest) {
